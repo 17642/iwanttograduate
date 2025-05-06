c23be065dc5ec245fe026bfa2ac9a64f0fe3d00c
상빈 <67950060+17642@users.noreply.github.com>
2025-05-06 15:41:55 +0900

    Update Vaildation.py

--- Vaildation.py
+++ Vaildation.py
@@ -11,20 +11,47 @@ from train import collate_fn
 import sys
 from colorama import init
 import time
+import argparse
+import numpy as np
+
+
+DEFAULT_MODEL_PATH = ".\\checkpoints\\best_model.pth"
+DEFAULT_DATA_PATH = ".\\valid_dataset"
+DEFAULT_VALIDATE_BATCH = 32
+
+LINE = '-'*32
+BLANK = ' '*64
+
+def print_with_ext(str):
+    print(str)
+    return str
 
 class Validation(CNNAudioClassifier):
-    def __init__(self, _modelPath, _dataPath, num_classes, max_Validation_length=-1):
+    def __init__(self, _modelPath, _dataPath, num_classes, max_Validation_length=-1, silent=False, txt_out = False):
         super().__init__(num_classes)
 
         self.modelPath = _modelPath
         self.dataPath = _dataPath
         self.ValidLen = max_Validation_length
+        self.silent=silent
+        self.outtxt = ""
+        self.txt_out = txt_out
 
         self.load_state_dict(torch.load(self.modelPath))
-        print(f"{'MODEL LOADED':<40}[{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))}]")
+        LOADSTR = f"{'MODEL LOADED':<40}[{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))}]"
+        self.ptxtout(False,LOADSTR)
+
+    def ptxtout(self,cls,str):
+        if not self.silent:
+            if cls:
+                self.clearandprint(str)
+            else:
+                print(str)
+        if self.txt_out:
+            self.outtxt += str+'\n'
 
     def clearandprint(self, str):
-        print(" "*64)
+        print(BLANK)
         sys.stdout.write("\033[1A")
         sys.stdout.flush()
         print(str)
@@ -35,68 +62,109 @@ class Validation(CNNAudioClassifier):
         correct = 0
         total = 0
 
+        conf_matrix = np.zeros((configs.NUM_CLASSES,configs.NUM_CLASSES),dtype=int)
+
         if self.ValidLen == -1 or self.ValidLen>len(dataloader):
             self.ValidLen = len(dataloader)
 
-        print(f"VALIDATION LEN SET TO {self.ValidLen}")
+        VALSETSTR = f"VALIDATION LEN SET TO {self.ValidLen}"
+        self.ptxtout(False,VALSETSTR)
 
-        print(f"{'START VALIDATION':<40}[{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))}]")
+
+        VALSTR = f"{'START VALIDATION':<40}[{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))}]"
+        self.ptxtout(False,VALSTR)
         i = 0
-        st = time.time()
         with torch.no_grad():
+            st = time.time()
             for inputs, labels in dataloader:
                 outputs = self(inputs)
                 if i == 0:
-                    print("-" * 32)
-                    print(f"{'Sample Output and Labels:':<40}[{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))}]\n{'OUTPUT SHAPE:':<20}{outputs.shape}\n{'LABEL SHAPE:':<20}{labels.shape}")
+                    self.ptxtout(False,LINE)
+                    OUTTXT = f"{'Sample Output and Labels:':<40}[{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))}]\n{'OUTPUT SHAPE:':<20}{outputs.shape}\n{'LABEL SHAPE:':<20}{labels.shape}"
+                    self.ptxtout(False,OUTTXT)
                 loss = torch.nn.CrossEntropyLoss()(outputs, labels)
                 pred = outputs.argmax(dim=1)
+
                 if i == 0:
-                    print("-" * 32)
-                    print(f"Sample pred and Label\n\n")
+                    self.ptxtout(False,LINE)
+                    self.ptxtout(False,f"Sample pred and Label\n\n")
                 if i < 3:
-                    self.clearandprint(f"{'[Pred]':<10} {pred[0]:<10}{'[Label]':<10}{labels[0]}")
+                    self.ptxtout(True,f"{'[Pred]':<10} {pred[0]:<10}{'[Label]':<10}{labels[0]}")
+
+                for s in range(len(labels)):
+                    conf_matrix[labels[s]][pred[s]] += 1
+
                 correct += (pred == labels).sum().item()
                 total += labels.size(0)
                 total_loss += loss.item()
                 i+=1
-                self.clearandprint(f"{i}/{self.ValidLen} {'iter':<10}...{i/(time.time()-st):.2f} iter/sec. {time.time()-st:.2f}s elapsed.")
+                ltstr = f"{i}/{self.ValidLen} {'iter':<10}...{i/(time.time()-st):.2f} iter/sec. {time.time()-st:.2f}s elapsed."
+                if not self.silent:
+                    self.clearandprint(ltstr)
                 if self.ValidLen<i+1:
+                    self.outtxt+=ltstr+'\n'
                     break
-                if i<self.ValidLen:
+                if i<self.ValidLen and not self.silent:
                     sys.stdout.write("\033[1A")
                     sys.stdout.flush()
                 
-        print("-" * 32)
-        print(f"{'VALIDATION FINISHED':<40}[{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))}]")
+        self.ptxtout(False,LINE)
+        self.ptxtout(False,f"{'VALIDATION FINISHED':<40}[{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))}]")
 
         avg_loss = total_loss/self.ValidLen
         accuracy = correct/total
 
-        return {"loss": avg_loss, "accuracy": accuracy}
+        return {"loss": avg_loss, "accuracy": accuracy},self.outtxt, conf_matrix
 
 
+def main(args):
 
+    out_log = ""
+    txtout = False
+    confmatrixout = False
+    if args.output is not None:
+        txtout = True
 
-def main():
+    if args.confusion_matrix is not None:
+        confmatrixout = True
 
-    MODEL_PATH = ".\\checkpoints\\best_model.pth"
-    DATA_PATH = ".\\valid_dataset"
-    VALIDATE_BATCH = 32
-    sets = -1
-    if(len(sys.argv)==2):
-        sets = int(sys.argv[1])
+    v = Validation(args.model_path, args.data_path, configs.NUM_CLASSES,args.iteration,args.silent,txtout)
+    validate_dataset = EmergencySoundDataset(args.data_path)
+    validate_dataloader = DataLoader(validate_dataset, batch_size=args.batch , shuffle=True, num_workers=4, collate_fn=collate_fn)
+    DLSTR = f"{'DATA LOADED':<40}[{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))}]"
+    out_log+=DLSTR+'\n'
+    if not args.silent:
+        print(DLSTR)
+    rst,logs, confusion_matrix = v.validate(validate_dataloader)
+    out_log+=logs
+    out_log+=print_with_ext(LINE)+'\n'
+    out_log+=print_with_ext(f"VALIDATION RESULT:\n\n{'LOSS:':<25}{rst['loss']:>6.4f}\n{'ACCURACY:':<24}{rst['accuracy']*100:>6.2f}%")+'\n'
+    out_log+=print_with_ext(LINE)+'\n'
 
-    v = Validation(MODEL_PATH, DATA_PATH, configs.NUM_CLASSES,sets)
-    validate_dataset = EmergencySoundDataset(DATA_PATH)
-    validate_dataloader = DataLoader(validate_dataset, batch_size=VALIDATE_BATCH , shuffle=True, num_workers=4, collate_fn=collate_fn)
-    print(f"{'DATA LOADED':<40}[{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))}]")
-    rst = v.validate(validate_dataloader)
-    print("-" * 32)
-    print(f"VALIDATION RESULT:\n\n{'LOSS:':<25}{rst['loss']:>6.4f}\n{'ACCURACY:':<24}{rst['accuracy']*100:>6.2f}%")
-    print("-" * 32)
+    if txtout:
+        with open(args.output,'w',encoding='utf-8') as f:
+            f.write(out_log)
+            f.close()
+
+    if confmatrixout:
+        #print(confusion_matrix)
+        confusion_matrix = np.array(confusion_matrix)
+        np.save(args.confusion_matrix,confusion_matrix)
+    
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog='Valitation',description='설명 아무거나 넣기')
+    parser.add_argument('-o','--output',action='store',default=None,type=str)
+    parser.add_argument('-m','--confusion-matrix',action='store',default=None,type=str)
+    parser.add_argument('-s','--silent',action='store_true',default=False)
+    parser.add_argument('-i','--iteration',action='store',default=-1,type=int)
+    parser.add_argument('-p','--model-path',action='store',default=DEFAULT_MODEL_PATH,type=str)
+    parser.add_argument('-p2','--data-path',action='store',default=DEFAULT_DATA_PATH,type=str)
+    parser.add_argument('-b','--batch',action='store',default=DEFAULT_VALIDATE_BATCH,type=int)
+    
+    args = parser.parse_args()
+
     init()
-    main()
+    main(args)
+
